21b6aa756f794dcd5aa1feebcd0ebd20aae118ae
Ashutosh Gupta <ashutosh.gupta@st.niituniversity.in>
2016-06-05 02:58:11 +0530

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+ 
+
+
+<?php  
+session_start();  
+  
+if(!$_SESSION['email'])  
+{  
+  
+    header("Location: login.php");//redirect to login page to secure the welcome page without login access.  
+}  
+  
+?>  
+  
+<html>  
+ <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Welcome Page</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="signin.css" rel="stylesheet">
+
+  </head> 
+  
+<body>  
+<nav class="navbar navbar-light bg-faded">
+  <ul class="nav navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="#">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Messages</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Favourites</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Cart</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="logout.php">Logout</a>
+    </li>
+  </ul>
+</nav>
+  
+</nav>
+</body>  
+  
+</html>
